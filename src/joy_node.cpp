6df8fed890d41601c52196ce0db2542b2b261c7f
Khasreto <adam-krawczyk@outlook.com>
2021-01-22 11:42:07 +0100

    update for mecanum and change speed button

--- src/joy_node.cpp
+++ src/joy_node.cpp
@@ -14,19 +14,22 @@ private:
 
     ros::NodeHandle nh_;
 
-    int linear_{1}, angular_{0}, omni_{2};
-    double l_scale_{0.5}, a_scale_{0.5};
+    int linear_x_{1}, linear_y_{3}, angular_z_{0};
+    double lin_x_scale_{0.5}, lin_y_scale_{0.5}, ang_z_scale_{0.5};
+
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
 };
 
-TeleopJoy::TeleopJoy() : linear_(1), angular_(0), omni_(2)
+TeleopJoy::TeleopJoy() : linear_x_(1), linear_y_(3), angular_z_(0)
 {
+    nh_.param("axis_linear_x", linear_x_, linear_x_);
+    nh_.param("axis_linear_y", linear_y_, linear_y_);
+    nh_.param("axis_angular_z", angular_z_, angular_z_);
 
-    nh_.param("axis_linear", linear_, linear_);
-    nh_.param("axis_angular", angular_, angular_);
-    nh_.param("scale_angular", a_scale_, a_scale_);
-    nh_.param("scale_linear", l_scale_, l_scale_);
+    nh_.param("scale_linear_x", lin_x_scale_, lin_x_scale_);
+    nh_.param("scale_linear_y", lin_y_scale_, lin_y_scale_);
+    nh_.param("scale_angular_z", ang_z_scale_, ang_z_scale_);
 
     // nh_.getParam("/scale_linear", l_scale_);
 
@@ -38,32 +41,35 @@ void TeleopJoy::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
 {
     geometry_msgs::Twist twist;
 
-    if (joy->buttons[0] == 1)
+    if (joy->buttons[5] == 1)
     {
-        a_scale_ = 1.0;
-        l_scale_ = 1.0;
+        lin_x_scale_ = 1.0;
+        lin_y_scale_ = 1.0;
+        ang_z_scale_ = 1.0;
     }
     else
     {
-        a_scale_ = 0.5;
-        l_scale_ = 0.5;
+        // double factor = (joy->axes[5] - 1)*(-1);
+        lin_x_scale_ = 0.5;
+        lin_y_scale_ = 0.5;
+        ang_z_scale_ = 0.5;
     }
 
-    twist.angular.z = a_scale_ * joy->axes[angular_];
-    twist.linear.x = l_scale_ * joy->axes[linear_];
-    twist.linear.y = l_scale_ * joy->axes[omni_];
+    twist.linear.x = lin_x_scale_ * joy->axes[linear_x_];
+    twist.linear.y = lin_y_scale_ * joy->axes[linear_y_];
+    twist.angular.z = ang_z_scale_ * joy->axes[angular_z_];
+
     // ROS_INFO("Twist msg:");
-    // ROS_INFO("x: [%f], y: [%f], z: [%f]", twist.linear.x, twist.linear.z, twist.angular.z);
+    // ROS_INFO("x: [%f], y: [%f], z: [%f]", twist.linear.x, twist.linear.y, twist.angular.z);
     // ROS_INFO("Scale factors:");
-    // ROS_INFO("linear: [%f], angular: [%f], omni: [%f]", a_scale_ , a_scale_, twist.linear.y);
+    // ROS_INFO("linear_x_dir: [%f], linear_y_dir: [%f], angular_z_dir: [%f]", lin_x_scale_ , lin_y_scale_, ang_z_scale_);
     vel_pub_.publish(twist);
 }
 
 int main(int argc, char **argv)
 {
     ROS_INFO("Init Node");
-    ros::init(argc, argv, "teleop_turtle");
+    ros::init(argc, argv, "robot_teleop");
     TeleopJoy teleop_joy;
-
     ros::spin();
 }
